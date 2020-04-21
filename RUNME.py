e1e5871e72a03ed8d107f9db4ae04cda74266517
nat <nat.foster@gmail.com>
2020-04-21 10:59:52 -0600

    name needs space

--- RUNME.py
+++ RUNME.py
@@ -1,13 +1,13 @@
 try:
     with open('Discover Weekly 20200420', 'r') as lol:
-        beat_this_one = ['abcdefghijklmnopqrstuvwxyz'.index(letter) for index, letter in enumerate(''.join(sorted((possibly_misformatted_line.lower().strip().replace(' ', '').split('?')[0].split('/')[-1] for possibly_misformatted_line in lol.read().split())))) if letter not in '1234567890' and index % 30 == 3]
+        beat_this_one = ['abcdefghijklmnopqrstuvwxyz '.index(letter) for index, letter in enumerate(''.join(sorted((possibly_misformatted_line.lower().strip().replace(' ', '').split('?')[0].split('/')[-1] for possibly_misformatted_line in lol.read().split())))) if letter not in '1234567890' and index % 30 == 3]
         try:
             with open('NAME', 'r') as garbled_name:
-                okay_ill_try = secret_name = ''.join(('abcdefghijklmnopqrstuvwxyz'[('abcdefghijklmnopqrstuvwxyz'.index(garbled_letter) - beat_this_one[index % len(beat_this_one)]) % 26] for index, garbled_letter in enumerate(garbled_name.read())))
-                print('Your secret name is', secret_name, 'or maybe it\'s still secret')
+                okay_ill_try = secret_name = ''.join(('abcdefghijklmnopqrstuvwxyz '[('abcdefghijklmnopqrstuvwxyz '.index(garbled_letter) - beat_this_one[index % len(beat_this_one)]) % 27] for index, garbled_letter in enumerate(garbled_name.read())))
+                print('My secret name is', secret_name, 'or maybe it\'s still secret')
         except Exception:
             secret_name = input('What is your secret name? ').lower()
-            garbledeegook = ''.join(('abcdefghijklmnopqrstuvwxyz'[('abcdefghijklmnopqrstuvwxyz'.index(secret_letter) + beat_this_one[index % len(beat_this_one)]) % 26] for index, secret_letter in enumerate(secret_name)))
+            garbledeegook = ''.join(('abcdefghijklmnopqrstuvwxyz '[('abcdefghijklmnopqrstuvwxyz '.index(secret_letter) + beat_this_one[index % len(beat_this_one)]) % 27] for index, secret_letter in enumerate(secret_name)))
             with open('NAME', 'w') as garbled_name:
                 garbled_name.write(garbledeegook)
 except Exception:
